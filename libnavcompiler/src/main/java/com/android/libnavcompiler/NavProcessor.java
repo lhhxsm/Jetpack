dc30e360b64b9acb1cc65c0c5d3d9ca5420fe819
lhhxsm <1187828603@qq.com>
2020-11-13 17:31:18 +0800

    常用模块创建

--- libnavcompiler/src/main/java/com/android/libnavcompiler/NavProcessor.java
+++ libnavcompiler/src/main/java/com/android/libnavcompiler/NavProcessor.java
@@ -1,10 +1,15 @@
 package com.android.libnavcompiler;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.android.libnavannotation.ActivityDestination;
 import com.android.libnavannotation.FragmentDestination;
 import com.google.auto.service.AutoService;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.lang.annotation.Annotation;
 import java.util.HashMap;
 import java.util.Set;
@@ -21,6 +26,8 @@ import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
+import javax.tools.FileObject;
+import javax.tools.StandardLocation;
 
 @AutoService(Processor.class)
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
@@ -54,6 +61,63 @@ public class NavProcessor extends AbstractProcessor {
             //并收集到destMap 这个map中。以此就能记录下所有的页面信息了
             handleDestination(fragmentElements, FragmentDestination.class, destMap);
             handleDestination(activityElements, ActivityDestination.class, destMap);
+
+            //app/src/main/assets
+            FileOutputStream fos = null;
+            OutputStreamWriter writer = null;
+            try {
+                //filer.createResource()意思是创建源文件
+                //我们可以指定为class文件输出的地方，
+                //StandardLocation.CLASS_OUTPUT：java文件生成class文件的位置，/app/build/intermediates/javac/debug/classes/目录下
+                //StandardLocation.SOURCE_OUTPUT：java文件的位置，一般在/ppjoke/app/build/generated/source/apt/目录下
+                //StandardLocation.CLASS_PATH 和 StandardLocation.SOURCE_PATH用的不多，指的了这个参数，就要指定生成文件的pkg包名了
+                FileObject resource = filer.createResource(StandardLocation.CLASS_OUTPUT, "", OUTPUT_FILE_NAME);
+                String resourcePath = resource.toUri().getPath();
+                messager.printMessage(Diagnostic.Kind.NOTE, "resourcePath:" + resourcePath);
+
+                //由于我们想要把json文件生成在app/src/main/assets/目录下,所以这里可以对字符串做一个截取，
+                //以此便能准确获取项目在每个电脑上的 /app/src/main/assets/的路径
+                String appPath = resourcePath.substring(0, resourcePath.indexOf("app") + 4);
+                String assetsPath = appPath + "src/main/assets/";
+
+                File file = new File(assetsPath);
+                if (!file.exists()) {
+                    file.mkdirs();
+                }
+
+                //此处就是稳健的写入了
+                File outPutFile = new File(file, OUTPUT_FILE_NAME);
+                if (outPutFile.exists()) {
+                    outPutFile.delete();
+                }
+                outPutFile.createNewFile();
+
+                //利用fastjson把收集到的所有的页面信息 转换成JSON格式的。并输出到文件中
+                String content = JSON.toJSONString(destMap);
+                fos = new FileOutputStream(outPutFile);
+                writer = new OutputStreamWriter(fos, "UTF-8");
+                writer.write(content);
+                writer.flush();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (writer != null) {
+                    try {
+                        writer.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                if (fos != null) {
+                    try {
+                        fos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
 
         return true;
